fcb99a389546cb4a0a2ff9460e20eb25d5932848
Tenkouu <anhabyr.922@gmail.com>
2021-04-09 22:12:44 +0800

    Added search controller.

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,36 @@
 require("@babel/polyfill");
 import Search from './model/search';
 
-let search = new Search('pasta');
+/**
+ * Web аппын төлөв
+ * - Хайлтын query, үр дүн
+ * - Тухайн үзүүлж байгаа жор
+ * - Лайкалсан жорууд
+ * - Захиалж байгаа жорын найрлагууд
+ */
 
-search.doSearch().then(r => console.log(r));
+const state = {};
+
+const controlSearch = async () => {
+    // 1. Вэбээс хайлтын түлхүүр үгийг гаргаж авна.
+    const query = 'pizza';
+
+    if(query){
+    // 2. Шинээр хайлтын обьектийг үүсгэж өгнө.
+        state.search = new Search(query);
+
+    // 3. Хайлт хийхэд зориулж дэлгэцийн UI-г бэлтгэнэ.
+
+    // 4. Хайлтыг гүйцэтгэнэ.
+        await state.search.doSearch();
+
+    // 5. Хайлтын үр дүнг дэлгэцэнд үзүүлнэ.
+        console.log(state.search.result);
+    }
+
+};
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault(),
+    controlSearch();
+});
